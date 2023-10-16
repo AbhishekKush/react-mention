4fdde47db5640e6fb0e8f8990075f96f0c5567fd
Abhishek Kushwaha <abhishek@datability.co>
2023-10-16 18:29:21 +0530

    Update data.json url

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   useEffect(() => {
     // Fetch users from data.json
-    fetch("../public/data.json")
+    fetch("data.json")
       .then((response) => response.json())
       .then((data) => {
         if (data ) {
